13d71f3dd361f32e52325aff1f081612cfe9b462
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2019-12-06 16:16:16 +0000

    refactoring game - complete

--- _assets/js/overview.js
+++ _assets/js/overview.js
@@ -22,6 +22,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // get baseurl for all site links
   const baseurl = window.location.pathname.split('/', 4).join('/');
+  // root game element
+  const game = document.querySelector('.js-game');
+  // special class names
+  const imageSelectClass = 'js-game-image';
+  const resetbtnClass = 'js-game-reset';
+  const submitbtnClass = 'js-game-submit';
+  const selectedClass = 'is-selected';
+  const maxSelectedClass = 'has-limit';
+  const localStorageId = 'rig-game-result';
 
   // data file
   const data = {
@@ -85,14 +94,6 @@ document.addEventListener('DOMContentLoaded', function() {
     ]
   };
 
-  // root game element
-  const game = document.querySelector('.js-game');
-
-  // special class names
-  const imageSelectClass = 'js-game-image';
-  const selectedClass = 'is-selected';
-  const maxSelectedClass = 'has-limit';
-
   //////////////////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////
   //  build game content
@@ -142,11 +143,72 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // ---------------------------------------------------------------------------
-  // ........
+  // build first step of game
   // ---------------------------------------------------------------------------
+  function buildStep01() {
+    game.innerHTML = `
+      <div class="container container--xxl vpad--xxl">
+        <div class="text--center">
+          <h2 class="title title--xl">Where is your romantic Germany?</h2>
+          <p class="width width--lg text--xxl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatibus hic asperiores similique cum dignissimos impedit explicabo nulla sit.</p>
+          <p class="width width--lg text--xxl text--bold">Choose 5 images</p>
+        </div>
+        <div class="space--md row row--constant-6-6 row--sm-4-4-4 row--lg-3-3-3-3 row--no-gutters">
+          ${buildImageGrid(data.cities, 'step01', 3)}
+        </div>
+        <div class="game-submit text--center space--lg"><a class="${submitbtnClass} btn btn--lg btn--red" href="#0">Submit Images</a></div>
+      </div>
+    `;
+    // add event listeners for each image
+    [...document.querySelectorAll(`.${imageSelectClass}`)].map(item =>
+      imageSelect(item, 'step01')
+    );
+    // add event for submit button
+    document
+      .querySelector(`.${submitbtnClass}`)
+      .addEventListener('click', e => {
+        e.preventDefault();
+        data.shortlist = findChosenCities(data.selection['step01']);
+        goToStep('step02');
+        resetGameScroll();
+      });
+  }
 
+  // ---------------------------------------------------------------------------
+  // build second step of game
+  // ---------------------------------------------------------------------------
+  function buildStep02() {
+    game.innerHTML = `
+      <div class="container container--xxl vpad--xxl">
+        <div class="text--center">
+          <p class="width width--lg text--xxl text--bold">Choose another 5 images</p>
+        </div>
+        <div class="space--md row row--constant-6-6 row--sm-4-4-4 row--lg-3-3-3-3 row--no-gutters">
+          ${buildImageGrid(data.shortlist, 'step02', 6)}
+        </div>
+        <div class="game-submit text--center space--lg"><a class="${submitbtnClass} btn btn--lg btn--red" href="#0">Submit Images</a></div>
+      </div>
+    `;
+    // add event listeners for each image
+    [...document.querySelectorAll(`.${imageSelectClass}`)].map(item =>
+      imageSelect(item, 'step02')
+    );
+    // add event for submit button
+    document
+      .querySelector(`.${submitbtnClass}`)
+      .addEventListener('click', e => {
+        e.preventDefault();
+        data.final = findChosenCities(data.selection['step02'])[0];
+        window.localStorage.setItem(localStorageId, data.final.id);
+        goToStep('result');
+        resetGameScroll();
+      });
+  }
+
+  // ---------------------------------------------------------------------------
+  // build final result of game
+  // ---------------------------------------------------------------------------
   function buildResult() {
-    game.innerHTML = null;
     game.innerHTML = `
       <div class="container container--xxl vpad--xxl">
         <div class="width width--xl text--center">
@@ -161,48 +223,23 @@ document.addEventListener('DOMContentLoaded', function() {
             <a href="${data.final.result.links.offer}" class="btn btn--red">See ${data.final.name} offers</a>
           </div>
           <div class="space--sm">
-            <a href="${data.final.result.links.more}" class="btn btn--outline btn--outline-red btn--sm">Find Out More</a> <a href="#" class="js-game-reset btn btn--outline btn--outline-red btn--sm">Choose again?</a>
+            <a href="${data.final.result.links.more}" class="btn btn--outline btn--outline-red btn--sm">Find Out More</a> <a href="#0" class="${resetbtnClass} btn btn--outline btn--outline-red btn--sm">Choose again?</a>
           </div>
         </div>
       </div>
     `;
-    resetGameButton();
+    // add event for reset button
+    document
+      .querySelector(`.${resetbtnClass}`)
+      .addEventListener('click', function(e) {
+        e.preventDefault();
+        resetGame();
+      });
   }
-
-  function buildStep01() {
-    game.innerHTML = null;
-    game.innerHTML = `
-      <div class="container container--xxl vpad--xxl">
-        <div class="text--center">
-          <h2 class="title title--xl">Where is your romantic Germany?</h2>
-          <p class="width width--lg text--xxl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatibus hic asperiores similique cum dignissimos impedit explicabo nulla sit.</p>
-          <p class="width width--lg text--xxl text--bold">Choose 5 images</p>
-        </div>
-        <div class="space--md row row--constant-6-6 row--sm-4-4-4 row--lg-3-3-3-3 row--no-gutters">
-          ${buildImageGrid(data.cities, 'step01', 3)}
-        </div>
-      </div>
-    `;
-    // add event listeners here
-  }
-
-  const content = {
-    step02: `
-      <div class="container container--xxl vpad--xxl">
-        <div class="text--center">
-          <p class="width width--lg text--xxl text--bold">Choose another 5 images</p>
-        </div>
-        <div class="space--md row row--constant-6-6 row--sm-4-4-4 row--lg-3-3-3-3 row--no-gutters"></div>
-      </div>
-    `,
-    submit: `
-      <div class="game-submit text--center space--lg"><a class="btn btn--lg btn--red" href="#">Submit Images</a></div>
-    `
-  };
 
   //////////////////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////
-  //  game state changes
+  //  game functions
   //////////////////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////
 
@@ -232,6 +269,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // show each step of the game and populate
   // ---------------------------------------------------------------------------
   function goToStep(stepName) {
+    // build each step
     switch (stepName) {
     case 'result':
       buildResult();
@@ -239,22 +277,12 @@ document.addEventListener('DOMContentLoaded', function() {
     case 'step01':
       buildStep01();
       break;
-    default:
-      console.log('dis a default');
+    case 'step02':
+      buildStep02();
+      break;
     }
-
-    // create a marker for which step of the game is current
-    game.setAttribute('data-step', stepName);
-
-    // console.log(game);
-
-    // so in this context step name is the string 'result'
-
-    // build all of the dynamic bits that are needed
-    // grab the step layout html
-    // add in the dynamic bits to the layout
-    // add the final layout to the page
-    // add in all of the event listeners and such
+    // remove global class for image selection limit
+    game.classList.remove(maxSelectedClass);
   }
 
   // ---------------------------------------------------------------------------
@@ -265,9 +293,17 @@ document.addEventListener('DOMContentLoaded', function() {
     delete data.final;
     delete data.shortlist;
     delete data.selection;
+    window.localStorage.removeItem(localStorageId);
     // show first step and scroll
     goToStep('step01');
     resetGameScroll();
+  }
+
+  // ---------------------------------------------------------------------------
+  // reset game scroll position
+  // ---------------------------------------------------------------------------
+  function resetGameScroll() {
+    document.documentElement.scrollTop = game.offsetTop;
   }
 
   //////////////////////////////////////////////////////////////////////////////
@@ -305,69 +341,11 @@ document.addEventListener('DOMContentLoaded', function() {
         }
       }
       if (data.selection[step].length === 5) {
-        target.classList.add(maxSelectedClass);
+        game.classList.add(maxSelectedClass);
       } else {
-        target.classList.remove(maxSelectedClass);
+        game.classList.remove(maxSelectedClass);
       }
     });
-  }
-
-  // ---------------------------------------------------------------------------
-  // submit selected images
-  // ---------------------------------------------------------------------------
-  // button.addEventListener('click', e => {
-  //   e.preventDefault();
-  //   const currentStep = game.dataset.step;
-  //   if (currentStep === 'step01') {
-  //     // find winners, clear html, create step 2 images
-  //     data.shortlist = findChosenCities(data.selection[currentStep]);
-  //     game.innerHTML = null;
-  //     intro.append(secondTitle);
-  //     title.remove();
-  //     description.remove();
-  //     instruction.remove();
-  //     target.classList.remove(maxSelectedClass);
-  //     buildImageGrid(data.shortlist, 'step02', 6);
-  //   } else if (currentStep === 'step02') {
-  //     // find winner, clear html, show result
-  //     data.final = findChosenCities(data.selection[currentStep])[0];
-  //     window.localStorage.setItem('rig-game-result', data.final.id);
-
-  //     // would be good to save this final result on local storage, that way if people navigate away they can still get their resilt
-
-  //     // remove current image grid and show the result
-  //     target.classList.remove(maxSelectedClass);
-  //     game.remove();
-  //     container.innerHTML = null;
-  //     container.innerHTML = `
-  //     <div class="width width--xl text--center">
-  //       <h3 class="sub-title sub-title--sm">You should be heading for...</h3>
-  //       <h2 class="title title--xxxl">${data.final.name}</h2>
-  //       <div class="space--lg"></div>
-  //       <div class="bg-img bg-img--16-9" style="background-image:url('${data.final.result.image}')"></div>
-  //       <div class="space--sm"></div>
-  //       <p class="width width--lg text--xxl">${data.final.result.description}</p>
-  //       <div class="space--md"></div>
-  //       <div>
-  //         <a href="${data.final.result.links.offer}" class="btn btn--red">See ${data.final.name} offers</a>
-  //       </div>
-  //       <div class="space--sm">
-  //         <a href="${data.final.result.links.more}" class="btn btn--outline btn--outline-red btn--sm">Find Out More</a> <a href="#" class="js-game-reset btn btn--outline btn--outline-red btn--sm">Choose again?</a>
-  //       </div>
-  //     </div>
-  //     `;
-  //     resetGame();
-  //   }
-  //   resetGameScroll();
-  // });
-
-  function resetGameButton() {
-    document
-      .querySelector('.js-game-reset')
-      .addEventListener('click', function(e) {
-        e.preventDefault();
-        resetGame();
-      });
   }
 
   //////////////////////////////////////////////////////////////////////////////
@@ -375,90 +353,11 @@ document.addEventListener('DOMContentLoaded', function() {
   //  initialise game
   //////////////////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////
-
-  // so no more functions, this is the heart of the code that fucks with all of
-  // the moving parts. This is the run time, statements not functions
-
-  // the page loads
-  // the game element is already found
-
-  const previousResult = window.localStorage.getItem('rig-game-result');
+  const previousResult = window.localStorage.getItem(localStorageId);
   if (previousResult) {
     data.final = data.cities.find(item => item.id === previousResult); // capture results in global data
-
-    // goToStep('step01');
-    goToStep('result'); // show result step
+    goToStep('result');
   } else {
-    // show first step
-  }
-
-  // has there been a previous result?
-  // look in local storage
-  // show result page if so
-  // show first step if not
-
-  // // add all elements for the first step of the game
-  // function createFirstStep() {
-  //   // // combine all elments into the final html
-  //   // container.append(intro, game, bottom);
-  //   // intro.append(title, description, instruction);
-  //   // bottom.append(button);
-  //   // target.append(container);
-  //   // create the first step of images
-  //   buildImageGrid(data.cities, 'step01', 3);
-  // }
-  //  // initalise the game
-  // // this is where it needs to check the browser if there is an answer
-  // if (window.localStorage.getItem('rig-game-result')) {
-  //   console.log(
-  //     data.cities.find(
-  //       item => item.id === window.localStorage.getItem('rig-game-result')
-  //     )
-  //   );
-  // } else {
-  //   createFirstStep();
-  // }
-
-  // create all elements
-  // const container = document.createElement('div');
-  // const intro = document.createElement('div');
-  // const title = document.createElement('h2');
-  // const secondTitle = document.createElement('h2');
-  // const description = document.createElement('p');
-  // const instruction = document.createElement('p');
-  // const game = document.createElement('div');
-  // const bottom = document.createElement('div');
-  // const button = document.createElement('a');
-
-  // // add all classes & atrributes
-  // container.classList.add('container', 'container--xxl', 'vpad--xxl');
-  // intro.classList.add('text--center');
-  // title.classList.add('title', 'title--xl');
-  // secondTitle.classList.add('title', 'title--lg');
-  // description.classList.add('width', 'width--lg', 'text--lg');
-  // instruction.classList.add('width', 'width--lg', 'text--xxl', 'text--bold');
-  // game.classList.add(
-  //   'space--md',
-  //   'row',
-  //   'row--constant-6-6',
-  //   'row--sm-4-4-4',
-  //   'row--lg-3-3-3-3',
-  //   'row--no-gutters'
-  // );
-  // bottom.classList.add('game-submit', 'text--center', 'space--lg');
-  // button.classList.add('btn', 'btn--red');
-  // button.setAttribute('href', '#');
-
-  // // add all text
-  // title.innerText = 'Where is your romantic Germany?';
-  // secondTitle.innerText = 'Choose another 5 images';
-  // description.innerText =
-  //   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptatibus hic asperiores similique cum dignissimos impedit explicabo nulla sit.';
-  // instruction.innerText = 'Choose 5 images';
-  // button.innerText = 'Submit Images';
-
-  // reset window scroll position so title is always visible
-  function resetGameScroll() {
-    document.documentElement.scrollTop = game.offsetTop;
+    goToStep('step01');
   }
 });
