15f80681f01dbec05f11f7360578d9d0b095a65f
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2019-12-24 15:46:14 +0000

    video interactions

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -11,19 +11,27 @@ var videoImgPath = videoEl.attr('poster');
 
 // add thumbnail element
 video.addClass('thumbnail');
-video.prepend('<div class="thumbnail__bg bg--img" style="background-image: url(\'' + videoImgPath + '\');"><div class="thumbnail__play-icon"></div></div>');
+video.prepend(
+  '<div class="thumbnail__bg bg--img" style="background-image: url(\'' +
+    videoImgPath +
+    '\');"><div class="thumbnail__play-icon"></div></div>'
+);
 
-// create click event to play video
-video.on('click', function(){
-  // NEED TO BUILD THIS PROPERLY
-  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  // this is just a quick thing to show how it will work
-  // I will eventually add in transitions for it all so it is nice and smooth
-  // also need to consider the mobile behaviour as well
-  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  $('.thumbnail__bg').empty().remove();
-  videoEl.css('visibility', 'visible');
+// remove thumbnail and play video
+function playVideo() {
+  $('.thumbnail__bg').fadeOut(250);
+  videoEl.css({
+    visibility: 'visible',
+    opacity: '1',
+    transition: 'opacity 1.5s ease'
+  });
   videoEl[0].play();
+}
+
+// watch events to play video
+video.on('click', playVideo);
+video.on('keydown', e => {
+  if (e.code == 'Enter') playVideo();
 });
 
 
@@ -35,8 +43,12 @@ $('.js-image-attribution').each(function() {
   var $this = $(this);
   var contents = $this.html();
   $this.css('cursor', 'pointer');
-  $this.html('<img style="width: 15px; height: 15px;" src="' + window.location.pathname.split('/', 4).join('/') + '/_assets/img/icons/info.svg" alt="picture attribution information">');
-  $this.on('click',function(){
+  $this.html(
+    '<img style="width: 15px; height: 15px;" src="' +
+      window.location.pathname.split('/', 4).join('/') +
+      '/_assets/img/icons/info.svg" alt="picture attribution information">'
+  );
+  $this.on('click', function() {
     $this.html(contents);
     $this.css('cursor', 'auto');
   });
